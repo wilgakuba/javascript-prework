fef300093deeb17c4a4db7f5a9741b9439ed5218
Jakub Wilga <wilga327@wp.pl>
2023-02-01 14:46:05 +0100

    Add computer moves

--- js/script.js
+++ js/script.js
@@ -1,17 +1,22 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+if(randomNumber == 2){
+    computerMove = 'papier';
 }
 
-let computerMove ='kamień';
+if(randomNumber == 3){
+    computerMove = 'nożyce'
+}
 
-let playerMove ='papier';
+printMessage('Mój ruch to: ' + computerMove);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to' + playerMove +  ',to wygrywasz!');
 
 
