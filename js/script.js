b1f8f750f5e7f133aaae54b969f9313d8f4f4e82
Jakub Wilga <wilga327@wp.pl>
2023-02-01 14:52:10 +0100

    Add player moves

--- js/script.js
+++ js/script.js
@@ -19,4 +19,21 @@ if(randomNumber == 3){
 printMessage('Mój ruch to: ' + computerMove);
 
 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+}
+
+if(playerInput == '2'){
+    playerMove = 'papier';
+}
+
+if(playerInput == '3'){
+    playerMove = 'nożyce';
+}
+printMessage('Twój ruch to: ' + playerMove);
