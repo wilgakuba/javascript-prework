0413ee986810ebc272652fc07902c352ce137cbc
Jakub Wilga <wilga327@wp.pl>
2023-02-08 20:31:53 +0100

    Add const

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 {
   function playGame(){
     
     function clearMessages()
 
     const playerInput = getMoveName(randomNumber);
-=======
+
 function playGame(playerInput){
   
   function clearMessages()
@@ -21,9 +20,7 @@ function playGame(playerInput){
         printMessage('Nie znam ruchu o id ' + argMove + '.');
         return 'nieznany ruch';
     }
-}
->>>>>>> b5735bb85205dd713653d2941776293920b3d4e4
-
+  }
     const randomNumber = Math.floor(Math.random() * 3 + 1);
 
     console.log('Wylosowana liczba to: ' + randomNumber);
@@ -95,7 +92,6 @@ function playGame(playerInput){
       }
     }
   }
-<<<<<<< HEAD
   document.getElementById('play-rock').addEventListener('click', function(){
     playGame(1);
   });
@@ -105,69 +101,5 @@ function playGame(playerInput){
   document.getElementById('play-scissors').addEventListener('click', function(){
     playGame(3);
   });
-}
-=======
-
-  printMessage('Mój ruch to: ' + computerMove);
-
-  console.log('Gracz wpisał: ' + playerInput);
-
-  let playerMove = getMoveName(playerInput);
-
-  printMessage('Twój ruch to: ' + playerMove);
-
-  function displayResult(computerMove, playerMove){
-  
-    printMessage('Zagrałem ' + computerMove + ', a Ty ' + playerMove); 
-
-  if(computerMove == 'kamień' && playerMove == 'papier');
-  {
-      printMessage('Ty wygrywasz!');
-    }
-
-    if(computerMove == 'papier' && playerMove == 'nożyce') 
-    {
-      printMessage('Ty wygrywasz!');
-    }
-
-    if(computerMove == 'nożyce' && playerMove == 'kamień') 
-    {
-      printMessage('Ty wygrywasz!');
-    }
-    
-    
-    
-    if(computerMove == 'kamień' && playerMovee == 'nożyce');
-    {
-      printMessage('Przegrywasz!');
-    }
-  
-    if(computerMove == 'papier' && playerMove == 'kamień') 
-    {
-      printMessage('Przegrywasz!');
-    }
-  
-    if(computerMove == 'nożyce' && playerMove == 'papier') 
-    {
-      printMessage('Przegrywasz!');
-    }
-
-    if(computerMove == playerMove) {
-      printMessage('Remis!')
-    }
-
-    if(playerMove == 'nieznany ruch') {
-      printMessage('Błąd!')
-    }
   }
 }
-document.getElementById('play-rock').addEventListener('click', function(){
-  playGame(1);
-});
-document.getElementById('play-paper').addEventListener('click', function(){
-  playGame(2);
-});
-document.getElementById('play-scissors').addEventListener('click', function(){
-  playGame(3);
-});
->>>>>>> b5735bb85205dd713653d2941776293920b3d4e4
